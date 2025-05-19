21c75fa75bdb2f10a9efbc17090030fe07b8c672
Sidnei Teixeira <sidneimartinsteixeira@gmail.com>
2025-05-19 22:51:18 +0100

    add max concurrent clients; refactor server; add client auto removal

--- server/src/server.rs
+++ server/src/server.rs
@@ -14,7 +14,7 @@ const DEFAULT_CLIENT_PORT: usize = 7878;
 /// Creates a TcpListener using the local machine's IP address
 fn create_socket(port: usize) -> TcpListener {
     let local_ip = local_ip().unwrap();
-    println!("Created new socket: {local_ip}:{port}");
+    log::debug!("Created new socket: {local_ip}:{port}");
     TcpListener::bind(format!("{local_ip}:{port}")).unwrap()
 }
 
@@ -99,10 +99,12 @@ impl<A: Application + 'static> Server<A> {
     /// * `config` - Server configuration.
     /// * `app` - The application that will be called to handle incomming requests.
     pub fn build(config: ServerConfig, app: A) -> Result<Self, ServerError> {
-        let (sender, receiver) = mpsc::channel();
-        let clients = ClientPool::new(
-            sender,
-            Arc::new(receiver));
+        // Initialize logger with default settings
+        env_logger::init(); 
+        let (clients, receiver) = ClientPool::new(config.max_clients);
+
+        // await client termination commands
+        clients.start_termination_listener(receiver);
 
         Ok(Server {
             clients, 
@@ -129,26 +131,22 @@ impl<A: Application + 'static> Server<A> {
             // to that connection to the client
             match socket.accept() {
                 Ok((mut stream, addr)) => {
-                    println!("Received client connection");
+                    log::info!("Received client connection");
+                    // try adding new client to pool
+                    match self.clients.add(addr, Arc::clone(&self.app)) {
+                        Ok(port) => {
+                            let data = serde_json::to_vec(&NewClientResponse { 
+                                port,
+                                server_os: std::env::consts::OS.to_owned(), // send the server OS to client
+                            }).unwrap();
+                            stream.write_all(data.as_slice())
+                                .expect("Could not write into socket's client");
 
-                    if self.clients.len() < self.config.max_clients {
-                        println!("Connection accepted");
-                        let port = self.clients.add(addr, Arc::clone(&self.app));
-                        let data = serde_json::to_vec(&NewClientResponse { 
-                            port,
-                            server_os: std::env::consts::OS.to_owned(), // send the server OS to client
-                        }).unwrap();
-                        stream.write_all(data.as_slice())
-                            .expect("Could not write into socket's client");
+                        },
+                        Err(reason) => log::error!("{reason}")
                     }
-                    else {
-                        println!("Max clients reached - droping packet");
-                    }
-
                 }
-                Err(e) => {
-                    eprintln!("Could not accept connection! {}", e);
-                }
+                Err(e) => log::error!("Could not accept connection! {}", e)
             }
         }
     }
@@ -159,64 +157,108 @@ impl<A: Application + 'static> Server<A> {
 //            CLIENT CONNECTIONS
 //  ---------------------------------------
 
+const CLIENT_POOL_RESERVED_ID: usize = 0;
+
+/// Represents a termination command for a client thread running on
+/// the server upon the mobile client disconnects from the server's
+/// side client.
+#[derive(Debug)]
+struct Terminate {
+    client_id: usize,
+}
 /// Represents a pool of all client connections.
 struct ClientPool {
     client_id_counter: usize,
-    clients: HashMap<SocketAddr, Client>,
+
+    max_concurrent_clients_allowed: usize,
+
+    /// Store clients by id
+    /// Must be inside a mutex - there could be conflicts between
+    /// creating and removing clients since they run in different
+    /// threads
+    clients: Arc<Mutex<HashMap<usize, Client>>>,
+    
+    /// Passed to each client in the client pool
+    /// Used to send the `Terminate` command
     sender: mpsc::Sender<Terminate>,
-    receiver: Arc<mpsc::Receiver<Terminate>>
 }
 
 impl ClientPool {
 
-    pub fn new(
-        sender: mpsc::Sender<Terminate>, 
-        receiver: Arc<mpsc::Receiver<Terminate>>
-    ) -> ClientPool {
-
-        ClientPool {
+    pub fn new(max_clients: usize) -> (ClientPool, mpsc::Receiver<Terminate>) {
+        let (sender, receiver) = mpsc::channel();
+        let pool = ClientPool {
             // IDs must start at 1, to differ from base port used to receive new client requests
             client_id_counter: 1,
-            clients: HashMap::new(),
+            max_concurrent_clients_allowed: max_clients,
+            clients: Arc::new(Mutex::new(HashMap::new())),
             sender,
-            receiver
-        }
+        };
+
+        (pool, receiver)
     }
 
     /// Add a new client connection to the pool.
     ///
     /// For each new client, the id increases by 1, as well as the port.
     /// Client ids start at 0, and go up to max usize
-    pub fn add<A: Application + 'static>(&mut self, addr: SocketAddr, app: Arc<Mutex<A>>) -> usize {
+    pub fn add<A: Application + 'static>(&mut self, addr: SocketAddr, app: Arc<Mutex<A>>) -> Result<usize, String> {
+
+        if self.client_id_counter == self.max_concurrent_clients_allowed + 1 {
+            return Err("Maximum number of concurrent clients reached!".to_string());
+        }
 
         let new_client = Client::launch_new_client(
             addr,
             self.client_id_counter,
-            app
+            app,
+            self.sender.clone()
         );
         // insert new client only if it doesn't exist yet
         let port = new_client.port;
-        self.clients.entry(addr).or_insert(new_client);
+        let mut clients_lock = self.clients.lock().unwrap();
+        clients_lock.entry(self.client_id_counter).or_insert(new_client);
 
 
         self.client_id_counter+=1;
 
-        port
+        Ok(port)
     }
 
-    pub fn len(&self) -> usize {
-        self.clients.len()
+    /// Starts a thread that listens for client thread's termination requests.
+    /// These 'clients' are actually the threads running in the server. The requests
+    /// are used to remove the respective client from the pool.
+    pub fn start_termination_listener(&self, receiver: mpsc::Receiver<Terminate>) {
+        let clients = Arc::clone(&self.clients);
+
+        thread::spawn(move || {
+            while let Ok(Terminate { client_id }) = receiver.recv() {
+
+                // terminate listener
+                if client_id == CLIENT_POOL_RESERVED_ID {
+                    return;
+                }
+
+                let mut clients = clients.lock().unwrap();
+                match clients.remove(&client_id) {
+                    Some(client) => log::info!("Removed client with id {:?}: {:?}", client_id, client.address),
+                    None => log::error!("Tried to remove client {:?} but he wasn't in the pool!", client_id)
+                }
+            }
+        });
+    }
+
+    pub fn shutdown(&self) {
+        self.sender.send(Terminate { client_id: CLIENT_POOL_RESERVED_ID }).unwrap_err();
     }
 }
 
-/// Represents a Client handler.
-/// 
-/// Parses requests into the specified device
+
+#[derive(Copy, Clone)]
 struct Client {
     address: SocketAddr,
     id: usize,
     port: usize,
-    thread: thread::JoinHandle<()>
 }
 
 impl Client {
@@ -225,43 +267,62 @@ impl Client {
     /// Upon receiving an incoming message, send it to the application to be parsed.
     /// All information sent/received is in bytes.
     /// Calling this function creates a new thread that listens to requests.
-    fn launch_new_client<A: Application + 'static>(address: SocketAddr, id: usize, app: Arc<Mutex<A>>) -> Client {
+    fn launch_new_client<A: Application + 'static>(
+        address: SocketAddr, 
+        id: usize, 
+        app: Arc<Mutex<A>>,
+        remove_client: mpsc::Sender<Terminate>
+    ) -> Client {
+
         let port = DEFAULT_CLIENT_PORT + id;
         let socket = create_socket(port);
 
+        let client = Client{ address, id, port };
+
         // Create thread
-        let thread = thread::spawn(move || {
-            println!("Client created {:?} @ {:?}:{:?}", id, address, port);
+        thread::spawn(move || {
+            log::info!("Client created {:?} @ {:?}:{:?}", id, address, port);
             match socket.accept() {
                 Ok((stream, _)) => {
-                    Client::handle_request(stream, app);
+                    Client::handle_requests(&client, stream, app);
                 }
                 Err(e) => {
-                    eprintln!("Could not parse stream in Client {id}: {}", e);
+                    log::error!("Could not parse stream in Client {id}: {}", e);
                 }
             }
+
+            // remove the client upon error or connection termination
+            remove_client.send(Terminate{ client_id: id }).unwrap_err();
         });
-        Client { address, id, port, thread }
+        client.clone()
     }
 
     /// Handle incomming client inputs.
     ///
     /// Sends the received bytes up to the application to handle the input
-    fn handle_request(mut stream: TcpStream, app: Arc<Mutex<impl Application + 'static>>) {
+    fn handle_requests(client: &Client, mut stream: TcpStream, app: Arc<Mutex<impl Application + 'static>>) {
         // make the reads blocking - wait indefinetely for client input
         stream.set_read_timeout(None).expect("Could not set read timeout."); 
         loop {
             let mut bytes = [0 ; 1024];
-            let bytes_size = stream.read(&mut bytes).unwrap();
-            let bytes = &bytes[..bytes_size];
 
-            if bytes.is_empty() { continue };
-            if let ConnectionStatus::Disconnected = app.lock().unwrap().dispatch_to_device(bytes) {
-                println!("Client disconnected");
-                break;
+            match stream.read(&mut bytes) {
+                // Normal processing
+                Ok(bytes_size) => {
+                    if bytes.is_empty() { continue };
+
+                    let bytes = &bytes[..bytes_size];
+
+                    if let ConnectionStatus::Disconnected = app.lock().unwrap().dispatch_to_device(bytes) {
+                        log::info!("Client disconnected");
+                        break;
+                    }
+                }
+                // Error on connection (possibly abrupt disconnection by client)
+                Err(e) => {
+                    log::info!("Client {:?} listening at {:?} disconnected: {:?}", client.id, client.address, e);
+                }
             }
         }
     }
 }
-
-struct Terminate;
