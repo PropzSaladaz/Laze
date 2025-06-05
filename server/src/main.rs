3f4d16c91a2b3d6d8efb7512022c04f377a18d93
Sidnei Teixeira <sidneimartinsteixeira@gmail.com>
2025-06-05 09:16:11 +0100

    add async functionality to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 use mobile_controller::MobileController;
 use server::{Server, ServerConfig};
+use tokio::time::{sleep, Duration};
 
 mod mobile_controller;
 mod server;
@@ -8,16 +9,55 @@ mod keybinds;
 
 const PORT: usize = 7878;
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let config = ServerConfig::new(PORT, 10);
     let app = MobileController::new(
             1, 
             1,
             1,
             1500).unwrap();
-    let mut server = Server::build(config, app).unwrap();
-    server.start();
+
+    // server has started here
+    let mut server_comm = Server::start(config, app);
+
+    // emulates main thread communication with server
+    loop {
+        server_comm.send_request(server::ServerRequest::InitServer).await;
+        match server_comm.receive_response().await {
+            Some(server::ServerResponse::ServerStarted(p)) => {
+                println!("Server initialized successfully. {p}");
+            },
+            _ => {
+                println!("Unexpected response during initialization.");
+            }
+        }
+        sleep(Duration::from_secs(2)).await;
+
+        server_comm.send_request(server::ServerRequest::TerminateClient(2)).await;
+        match server_comm.receive_response().await {
+            Some(server::ServerResponse::ClientTerminated(t)) => {
+                println!("Client terminated successfully.{t}");
+            },
+            _ => {
+                println!("Unexpected response during client termination.");
+            }
+        }
+
+        sleep(Duration::from_secs(2)).await;
+
+        server_comm.send_request(server::ServerRequest::TerminateServer).await;
+        match server_comm.receive_response().await {
+            Some(server::ServerResponse::ServerTerminated) => {
+                println!("Server terminated successfully.");
+            },
+            _ => {
+                println!("Unexpected response during server termination.");
+            }
+        }
+        sleep(Duration::from_secs(2)).await;
+    
+    }
 }
 
 
