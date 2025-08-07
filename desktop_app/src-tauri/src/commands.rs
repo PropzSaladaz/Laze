04f9079f69858d518727c0d6b984bb6148774aa7
Sidnei Teixeira <sidneimartinsteixeira@gmail.com>
2025-08-07 11:12:58 +0100

    add event subscription in tauri app

--- desktop_app/src-tauri/src/commands.rs
+++ desktop_app/src-tauri/src/commands.rs
@@ -1,15 +1,19 @@
 use std::sync::{Arc, Mutex};
 
-use server::CommandSender;
+use server::{ServerHandler, ServerEvent};
+use tauri::Emitter;
+use tokio::sync::broadcast;
 
-type SharedCommunicator = Arc<Mutex<CommandSender>>;
+type SharedCommunicator = Arc<Mutex<ServerHandler>>;
 
 #[tauri::command]
-pub fn start_server(state: tauri::State<'_, SharedCommunicator>) -> String {
-    state.lock().unwrap().send_request(server::ServerRequest::InitServer).unwrap();
-    
-    match state.lock().unwrap().receive_response() {
+pub fn start_server(app_handle: tauri::AppHandle, state: tauri::State<'_, SharedCommunicator>) -> String {
+    let mut state = state.lock().unwrap();
+    state.start_server().unwrap();
+
+    match state.receive_response() {
         Ok(server::ServerResponse::ServerStarted(_)) => {
+            spawn_event_listener(state.subscribe_events(), app_handle);
             return format!("Server initialized successfully.");
         },
         _ => {
@@ -20,7 +24,7 @@ pub fn start_server(state: tauri::State<'_, SharedCommunicator>) -> String {
 
 #[tauri::command]
 pub fn stop_server(state: tauri::State<'_, SharedCommunicator>) -> String {
-    state.lock().unwrap().send_request(server::ServerRequest::TerminateServer).unwrap();
+    state.lock().unwrap().terminate_server().unwrap();
 
     match state.lock().unwrap().receive_response() {
         Ok(server::ServerResponse::ServerTerminated(_)) => {
@@ -30,4 +34,27 @@ pub fn stop_server(state: tauri::State<'_, SharedCommunicator>) -> String {
             return format!("Unexpected response during server termination.");
         }
     }
+}
+
+
+fn spawn_event_listener(
+    mut event_receiver: broadcast::Receiver<ServerEvent>,
+    app_handle: tauri::AppHandle,
+) {
+    // listen for server events
+    tauri::async_runtime::spawn(async move {
+        while let Ok(event) = event_receiver.recv().await {
+
+            // Handle server events here, e.g., log them or update UI
+            println!("Received server event: {:?}", event);
+            match event {
+                ServerEvent::ClientAdded(client_info) => {
+                    app_handle.emit("client-added", client_info).unwrap();
+                }
+                ServerEvent::ClientRemoved(client_info) => {
+                    app_handle.emit("client-removed", client_info).unwrap();
+                }
+            }
+        }
+    });
 }
