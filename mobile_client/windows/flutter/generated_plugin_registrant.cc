d490940fee6904aa62041b2fb413420c4a413a56
Sidnei Teixeira <sidneimartinsteixeira@gmail.com>
2026-02-18 18:11:06 +0000

    feat(client): add flutter_volume_controller dependency

--- mobile_client/windows/flutter/generated_plugin_registrant.cc
+++ mobile_client/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
+#include <flutter_volume_controller/flutter_volume_controller_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
+  FlutterVolumeControllerPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterVolumeControllerPluginCApi"));
 }
