1165a1d5790988580c1a910ecacc4c17875d0e15
Sidnei Teixeira <sidneimartinsteixeira@gmail.com>
2026-04-26 17:40:04 +0100

    Add url_launcher dependency for opening external download URLs.

--- mobile_client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile_client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import connectivity_plus
 import device_info_plus
 import flutter_volume_controller
 import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterVolumeControllerPlugin.register(with: registry.registrar(forPlugin: "FlutterVolumeControllerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- mobile_client/windows/flutter/generated_plugin_registrant.cc
+++ mobile_client/windows/flutter/generated_plugin_registrant.cc
@@ -8,10 +8,13 @@
 
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <flutter_volume_controller/flutter_volume_controller_plugin_c_api.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
   FlutterVolumeControllerPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterVolumeControllerPluginCApi"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
